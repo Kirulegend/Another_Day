b1620792c5aa8ae05a7a1d04139f6acd8d4d757b
ITISKIRU <kiranmoulikodidasugp@gmail.com>
2026-01-08 00:54:58 +0530

    Extra Packs
    Added Idli Pack, with textures and animation.

---
+++ Assets/Resources/Scripts/Batter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ITISKIRU
+{
+    public class Batter : MonoBehaviour, Interactable
+    {
+
+        public void OnInteract(string Msg)
+        {
+            
+        }
+
+        int currentPercent = 0;
+
+
+    }
+}
